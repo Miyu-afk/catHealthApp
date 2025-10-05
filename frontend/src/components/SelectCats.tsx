7f73b69919703877d5132a1b4d4e995e7a6957c8
Miyu Shimokawa <kooooohalulu@gmail.com>
2025-10-05 13:41:26 +0900

    猫の選択ができるように設定

---
+++ frontend/src/components/SelectCats.tsx
@@ -0,0 +1,42 @@
+interface CatManagement {
+  id: number;
+  name: string;
+  mood: boolean | null;
+  poop: boolean | null;
+  meal: boolean | null;
+  vitality: number;
+  record: string;
+  owner_id: number;
+}
+
+interface SelectCatsProps {
+  catList: number[];
+}
+
+const SelectCats = (props: SelectCatsProps) => {
+  if (!props.catList) {
+    return null;
+  }
+  return (
+    <>
+      <div className="dropdown dropdown-bottom dropdown-center">
+        <div tabIndex={0} role="button" className="btn m-1">
+          どのこにする？
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li>
+            <a>{props.catList}</a>
+          </li>
+          <li>
+            <a>{props.catList}</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default SelectCats;
